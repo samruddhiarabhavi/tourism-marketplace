333969c84da35d5d0199bc52c6f155a1f413870d
samruddhiarabhavi <samruddhiarabhavi@gmail.com>
2026-06-18 14:14:56 +0530

    destination

--- tourism-backend/server.js
+++ tourism-backend/server.js
@@ -7,6 +7,49 @@ app.use(express.json())
 app.get("/",(req,res)=>{
 res.send("tourism api is running");
 });
+app.get("/destinations", (req, res) => {
+
+    const sql = "SELECT * FROM destinations";
+
+    db.query(sql, (err, result) => {
+
+        if (err) {
+            return res.status(500).json(err);
+        }
+
+        res.json(result);
+});
+});
+app.post("/destinations",(req,res)=>{
+    const {name, location, price} = req.body;
+    const sql ="INSERT INTO destinations (name, location, price) VALUES (?, ?, ?)";
+    db.query(sql,[name,location,price],(err, result)=>{
+        if(err){
+            return res.status(500).json(err);
+        }
+        res.json({message:"Destination added successfully"})
+    })
+})
+app.delete("/destinations/:id", (req, res) => {
+
+  const id = req.params.id;
+
+  const sql =
+    "DELETE FROM destinations WHERE id = ?";
+
+  db.query(sql, [id], (err, result) => {
+
+    if (err) {
+      return res.status(500).json(err);
+    }
+
+    res.json({
+      message: "Destination Deleted"
+    });
+
+  });
+
+});
 app.listen(5000,()=>{
     console.log("server is running on port 5000");
 });
--- tourism-frontend/src/pages/Destinations.jsx
+++ tourism-frontend/src/pages/Destinations.jsx
@@ -1,5 +1,99 @@
+import { useEffect, useState } from "react";
 function Destinations() {
+  
+    const [destinations, setDestinations] = useState([]);
+    const [name, setName] = useState("");
+    const [location, setLocation] = useState("");
+    const [price, setPrice] = useState("");
+
+    useEffect(() => {
+        fetch("http://localhost:5000/destinations")
+            .then(res => res.json())
+            .then(data => setDestinations(data));
+    }, []);
+    const addDestination = async () => {
+      if (!name || !location || !price) {
+  alert("Please fill all fields");
+  return;
+}
+
+  const response = await fetch(
+    "http://localhost:5000/destinations",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        name,
+        location,
+        price
+      })
+    }
+  );
+
+  const data = await response.json();
+
+  alert(data.message);
+
+  setName("");
+  setLocation("");
+  setPrice("");
+
+  fetch("http://localhost:5000/destinations")
+    .then(res => res.json())
+    .then(data => setDestinations(data));
+};
+const deleteDestination = async (id) => {
+
+  const response = await fetch(
+    `http://localhost:5000/destinations/${id}`,
+    {
+      method: "DELETE"
+    }
+  );
+
+  const data = await response.json();
+
+  alert(data.message);
+
+  fetch("http://localhost:5000/destinations")
+    .then(res => res.json())
+    .then(data => setDestinations(data));
+};
     return(
-        <h1>Destination</h1>
-    )}
-    export default Destinations;
+        
+        <div>
+            <h1>Destinations</h1>
+            <h2>Add Destination</h2>
+            <input type="text" placeholder="destination name"value={name} onChange={(e) => setName(e.target.value)} />
+            <input type="text" placeholder="destination location"value={location} onChange={(e) => setLocation(e.target.value)} />
+            <input type="number" placeholder="destination price"value={price} onChange={(e) => setPrice(e.target.value)} />
+
+            <button onClick={addDestination}>
+  Add Destination
+</button>
+            <h2>Destination List</h2>
+
+      {destinations.map((item) => (
+        <div key={item.id}>
+          <h3>{item.name}</h3>
+          <p>{item.location}</p>
+          <p>₹ {item.price}</p>
+          <button
+      onClick={() => deleteDestination(item.id)}
+    >
+      Delete
+    </button>
+          
+        </div>
+      ))}
+      
+
+    </div>
+  );
+}
+
+
+
+export default Destinations;
