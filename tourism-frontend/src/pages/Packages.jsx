a0997095d993082eab2c0466d991f891ef6a7264
samruddhiarabhavi <samruddhiarabhavi@gmail.com>
2026-06-29 13:24:33 +0530

    pack

--- tourism-frontend/src/pages/Packages.jsx
+++ tourism-frontend/src/pages/Packages.jsx
@@ -1,6 +1,46 @@
+import { useEffect, useState } from "react";
 function Packages(){
-    return (
-        <h1>Packages</h1>
-    )
+       const [packages, setPackages] = useState([]);
+    const [name, setName] = useState("");
+    const [location, setLocation] = useState("");
+    const [duration, setDuration] = useState("");
+    const [price, setPrice] = useState("");
+    const[editId, setEditId] = useState(null);
+
+    useEffect(()=>{
+        fetch("https://localhost:5000/packages")
+        .then(res => res.json())
+        
+        .then(data => setPackages(data));
+    },[])
+    const addPackage = async() => {
+        if(!name || !location || !price || !duration){
+            alert("fill all the details");
+            return;
+        }
+        const response = await fetch(
+            "https://localhost:5000/packages"
+            {
+                method: "POST",
+                header:{
+                    "Content-type": "application/json"
+                },
+                body:JSON.stringify({
+                    name,
+                    location,
+                    price,
+                    duration
+                })
+            }
+        );
+        const data = await response.json();
+        alert(data.message);
+        setDuration(""),
+        setLocation(""),
+        setPrice(""),
+        setName("")
+
+
+    }
+
 }
-export default Packages;
